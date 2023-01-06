185f2ff4c521c741d238604f917a2d3a0c4b7e1b
rizki <mohamadrizki8@gmail.com>
2023-01-06 15:54:43 +0700

    add role to user

--- src/main/java/com/yanti/koperasi/model/User.java
+++ src/main/java/com/yanti/koperasi/model/User.java
@@ -14,6 +14,7 @@ public class User {
     private String nomorHandphone;
     private String username;
     private String password;
+    private String role = "Customer";
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
